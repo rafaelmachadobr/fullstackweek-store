7d027c965f670415deeb0447c26e11b15292604d
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-10-31 12:44:59 -0300

    feat: ensure recommended products are shown on product details page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,6 @@
 import { prismaClient } from "@/lib/prisma";
 import Categories from "./components/categories";
-import ProductList from "./components/product-list";
+import ProductList from "../../components/ui/product-list";
 import PromoBanner from "./components/promo-banner";
 import SectionTitle from "./components/section-title";
 
--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import ProductList from "@/components/ui/product-list";
 import { computeProductTotalPrice } from "@/helpers/product";
 import { prismaClient } from "@/lib/prisma";
 import ProductImages from "./components/product-images";
@@ -16,14 +17,28 @@ const ProductDetailsPage = async ({
     where: {
       slug: slug,
     },
+    include: {
+      category: {
+        include: {
+          products: {
+            where: {
+              slug: {
+                not: slug,
+              },
+            },
+          },
+        },
+      },
+    },
   });
 
   if (!product) return null;
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-8 pb-8">
       <ProductImages imageUrls={product.imageUrls} name={product.name} />
       <ProductInfo product={computeProductTotalPrice(product)} />
+      <ProductList products={product.category.products} />
     </div>
   );
 };
